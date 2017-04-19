2990ad827f4260680d0e127caac420ccea3f0772
Kevin D. Wolf <kevinw@software-logistics.com>
2017-04-19 17:24:22 -0400

    fix compile bug....mabye?

--- src/lagovista.iot.web.common/Attributes/SystemAdminAttribute.cs
+++ src/lagovista.iot.web.common/Attributes/SystemAdminAttribute.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using LagoVista.IoT.Web.Common.Claims;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 
 namespace LagoVista.IoT.Web.Common.Attributes
 {
-    class SystemAdminAttribute
+    public class SystemAdminAttribute : ActionFilterAttribute
     {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+
+            var ctrlDescriptor = context.ActionDescriptor as ControllerActionDescriptor;
+            if (ctrlDescriptor != null &&
+                ctrlDescriptor.ControllerName == "Account" &&
+                ctrlDescriptor.ActionName == "LogOff")
+            {
+                return;
+            }
+
+            if (context.HttpContext.User != null
+                && context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                if (context.HttpContext.User.HasClaim(ClaimsPrincipalFactory.EmailVerified, true.ToString()) &&
+                    context.HttpContext.User.HasClaim(ClaimsPrincipalFactory.PhoneVerfiied, true.ToString()))
+                {
+                    var orgId = context.HttpContext.User.Claims.Where(claim => claim.Type == ClaimsPrincipalFactory.CurrentOrgId).FirstOrDefault();
+                    if ((((string)context.RouteData.Values["controller"]).ToLower() != "verifyidentity") &&
+                            (orgId == null || String.IsNullOrEmpty(orgId.Value) || orgId.Value == ClaimsPrincipalFactory.None) &&
+                            !((ctrlDescriptor.ControllerName == "Organization" && ctrlDescriptor.ActionName == "Create")))
+                    {
+                        context.Result = new RedirectToActionResult("Create", "Organization", null);
+                    }
+                }
+                else if (((string)context.RouteData.Values["controller"]).ToLower() != "NotAuthorized")
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary(new { Controller = "NotAuthorized", View="SystemAdmin" }));
+                }
+            }
+        }
     }
 }
