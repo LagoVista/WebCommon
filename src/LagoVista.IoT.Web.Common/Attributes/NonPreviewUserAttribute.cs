f09fe2b8ecf43f97765a92059137230c5a9c5c62
Kevin D Wolf <kevin@DESKTOP-R4064KP.(none)>
2017-10-23 09:21:27 -0400

    add SMS notify and health reporting to MCP, add set preview user and attribute to user admin

---
+++ src/LagoVista.IoT.Web.Common/Attributes/NonPreviewUserAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using LagoVista.AspNetCore.Identity.Managers;
+using LagoVista.Core.Validation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LagoVista.IoT.Web.Common.Attributes
+{
+    public class NonPreviewUserAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            base.OnActionExecuted(context);
+
+            if (context.HttpContext.User != null && context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                if (!context.HttpContext.User.HasClaim(ClaimsFactory.IsPreviewUser, false.ToString()))
+                {
+                    context.HttpContext.Response.StatusCode = 403;
+                    context.HttpContext.Response.Headers.Clear();
+                    var result = new InvokeResult();
+                    result.Errors.Add(new ErrorMessage("Action Requires Org Admin Role"));
+                    context.Result = new JsonResult(result);
+                }
+            }
+            /* else - Can't think of where the Authorize attribute wouldn't take care of this case */
+        }
+    }
+}
