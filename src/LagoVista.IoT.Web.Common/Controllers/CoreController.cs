e82276d7311fede3f309fa848eae46d1f21023c1
Kevin D. Wolf <kevinw@software-logistics.com>
2024-07-06 08:09:12 -0400

    schedule factory

---
+++ src/LagoVista.IoT.Web.Common/Controllers/CoreController.cs
@@ -0,0 +1,21 @@
+﻿using LagoVista.Core.Models.UIMetaData;
+using LagoVista.IoT.Logging.Loggers;
+using LagoVista.UserAdmin.Models.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LagoVista.IoT.Web.Common.Controllers
+{
+    public class CoreController : LagoVistaBaseController
+    {
+        public CoreController(UserManager<AppUser> userManager, IAdminLogger logger) : base(userManager, logger)
+        {
+        }
+
+        [HttpGet("/api/core/schedule/factory")]
+        public DetailResponse<Core.Models.Schedule> CreateSchedule()
+        {
+            return DetailResponse<Core.Models.Schedule>.Create();
+        }
+    }
+}
